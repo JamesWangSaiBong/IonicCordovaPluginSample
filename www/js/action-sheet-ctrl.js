9e24e8a83d22eb89c4ed21ddd1fdeda97b8698a0
James Wang <jwsb0702@gmail.com>
2016-06-15 23:09:44 -0400

    Created action sheet page and link to it from main page

--- www/js/action-sheet-ctrl.js
+++ www/js/action-sheet-ctrl.js
@@ -1,5 +1,5 @@
 'use strict'
 
 app.controller("ActionSheetController", function($scope) {
-	
+	$scope.dummy = "Hello World!"
 });
--- www/js/states.js
+++ www/js/states.js
@@ -2,6 +2,10 @@
 app.config(function($stateProvider, $urlRouterProvider) {
 
 	$stateProvider
+		.state('main', {
+			url: '/',
+			templateUrl: 'views/main.html'
+		})
 		.state('actionSheet', {
 			url: '/action-sheet',
 			templateUrl: 'views/action-sheet.html',
@@ -9,6 +13,6 @@ app.config(function($stateProvider, $urlRouterProvider) {
 		})
 
 		// if none of the above states are matched, use this as the fallback
-		$urlRouterProvider.otherwise('/tab/dash');
+		$urlRouterProvider.otherwise('/');
 
 });
